cd6bb89497a07fee97f4dea88c0ba0ebf4da8dba
ainacb <ainacb.93@gmail.com>
2024-10-24 19:26:09 +1100

    Update helloworld.js
    basic function - return
    +-*/ and name
    basic concatenate

--- module1/backend/helloworld.js
+++ module1/backend/helloworld.js
@@ -6,12 +6,38 @@ sayHello();
 sayHello();
 sayHello();
 
-function sum(a, b) {
-  console.log(a + b);
-}
-sum(10, 15);
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+// sum(10, 15);
 
 function sum(a, b) {
   return a + b;
 }
 console.log(sum(5, 1));
+
+function addNumber(a, b) {
+  return a + b;
+}
+console.log(addNumber(11, 48));
+
+function subtractNumber(a, b) {
+  return a - b;
+}
+console.log(subtractNumber(45, 15));
+
+function multipleNumber(a, b) {
+  return a * b;
+}
+console.log(multipleNumber(20, 3));
+
+function divideNumber(a, b) {
+  return a / b;
+}
+console.log(divideNumber(63, 9));
+
+function sayHello(firstName) {
+  console.log("Hello" + " " + firstName);
+} //   Concatenate strings
+
+sayHello("Aina!!!");
