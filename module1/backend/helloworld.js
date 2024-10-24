999a1d12aa2d61cd2fc3c0a884158b0c259914e8
ainacb <ainacb.93@gmail.com>
2024-10-24 18:40:17 +1100

    Update helloworld.js

--- module1/backend/helloworld.js
+++ module1/backend/helloworld.js
@@ -1,1 +1,17 @@
 console.log("Hello Aina");
+function sayHello() {
+  console.log("Hello from inside of function");
+}
+sayHello();
+sayHello();
+sayHello();
+
+function sum(a, b) {
+  console.log(a + b);
+}
+sum(10, 15);
+
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(5, 1));
