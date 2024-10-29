7f913cafd4bca7f9bc48cc65a89cf86f2ba9d39b
ainacb <ainacb.93@gmail.com>
2024-10-29 20:09:53 +1100

    Update helloworld.js
    Arrays, JSON, var let const

--- module1/backend/helloworld.js
+++ module1/backend/helloworld.js
@@ -48,3 +48,114 @@ function Hello(firstName) {
 // console.log("Hello " + firstName); // same result as above
 
 Hello("Aina!!!");
+
+let helloText = "say Hi";
+let check = 4;
+if (check > 3) {
+  let hello = "say Hello instead";
+  console.log(hello); // "say Hello instead"
+}
+// console.log(hello); // hello is not defined
+
+//Arrays
+
+let emptyScores = new Array();
+// or
+let scores = ["A+", 95, "C-", 55, 83, 71, "B+"];
+
+let mountains = ["Everest", "Fuji", "Nanga Parbat"];
+console.log(mountains[0]); // 'Everest'
+console.log(mountains[1]); // 'Fuji'
+console.log(mountains[2]); // 'Nanga Parbat'
+
+mountains[1] = "Kilimanjaro";
+console.log(mountains); // [ 'Everest', 'Kilimanjaro', 'Nanga Parbat' ]
+
+//push method
+
+let seas = ["Black Sea", "Caribbean Sea", "North Sea", "Baltic Sea"];
+console.log(seas); // [ 'Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea' ]
+seas.push("Red Sea");
+console.log(seas); // [ 'Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea', 'Red Sea' ]
+
+//unshift
+
+seas.unshift("Adriatic Sea");
+console.log(seas); // [ 'Adriatic Sea', 'Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea', 'Red Sea' ]
+
+//pop
+
+let rivers = ["Mississippi", "Amazon", "Nile"];
+let lastRiver = rivers.pop();
+console.log(lastRiver); // Nile
+console.log(rivers); // [ 'Mississippi', 'Amazon' ]
+
+//shift
+
+let firstRiver = rivers.shift();
+console.log(firstRiver); // Mississippi
+console.log(rivers); // Amazon
+
+let volcanoes = ["Mount Vesuvius", "Mount Etna", "Mount Fuji"];
+let fujiIndex = volcanoes.indexOf("Mount Fuji");
+console.log(fujiIndex); // 2 (indexes start at 0)
+
+let numVolcanoes = volcanoes.length;
+console.log(numVolcanoes); // 3
+
+//5 element array
+
+let weather = ["Sunny", "Cloudy", "Windy", "Rainy", "Snowy"];
+
+console.log(weather[0]);
+console.log(weather[1]);
+console.log(weather[2]);
+console.log(weather[3]);
+console.log(weather[4]);
+
+//replace value
+
+weather[1] = "Foggy";
+console.log(weather[1]);
+
+weather[4] = "Stormy";
+console.log(weather[4]);
+
+//new element at beginning of array
+
+weather.unshift("Blizzard");
+console.log(weather);
+
+//remove element at end
+
+weather.pop();
+console.log(weather);
+
+const user = {
+  first_name: "Aina",
+  last_name: "Bautista",
+  age: "30",
+  followers: "987",
+};
+
+console.log(user.first_name); // Aina
+console.log(user.age); //30
+
+user.location = "Australia";
+
+const book = {
+  title: "El Filibusterismo",
+  description:
+    "The Reign of Greed, is the second novel written by José Rizal. It is the sequel to Noli Me Tángere and, like the first book, was written in Spanish. It was first published in 1891 in Ghent.",
+  author: "Jose Rizal",
+  number_of_pages: "368", // should have no space in between, eg: number of page should be number_of_pages or numberOfPage
+};
+
+console.log(book.title);
+console.log(book.description);
+console.log(book.author);
+console.log(book.number_of_pages);
+console.log(book);
+
+book.description = "The Subversive";
+console.log(book.description);
