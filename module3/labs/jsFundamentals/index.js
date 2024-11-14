3da86e798076c532ee893a790b07baf9f5a6d2c9
ainacb <ainacb.93@gmail.com>
2024-11-14 21:03:41 +1100

    module 3 question 1-5 done

--- module3/labs/jsFundamentals/index.js
+++ module3/labs/jsFundamentals/index.js
@@ -110,10 +110,50 @@ console.log(result);
 
 // 5. Rewrite the following function using: a) function expression syntax, and b) arrow function syntax. Test each version to make sure they work the same.
 
-// let name = Aina;
+function getGreeting(name) {
+  return "Hello " + name + "!";
+}
 
-// function getGreeting(name) {
-//   return "Hello " + name + "!";
-// }
+console.log(getGreeting("Aina"));
 
-// `Hello my name is ${firstName}!`;
+// a. function expression
+const getGreetingFE = function (name) {
+  return "Hello " + name + "!";
+};
+
+console.log(getGreetingFE("Aina"));
+
+// b. function arrow
+
+const getGreetingFA = (name) => "Hello " + name + "!";
+
+console.log(getGreetingFA("Aina"));
+
+// 6.
+// a) Complete the inigo object by adding a lastName property and including it in the greeting.
+// b) Complete getCatchPhrase so that if the person argument has 6 fingers, it instead prints his famous catch phrase to the console.
+// HINT: see https://www.imdb.com/title/tt0093779/characters/nm0001597.
+// c) Update getCatchPhrase to use arrow function syntax and a conditional operator.
+
+const westley = {
+  name: "Westley",
+  numFingers: 5,
+};
+const rugen = {
+  name: "Count Rugen",
+  numFingers: 6,
+};
+
+const inigo = {
+  firstName: "Inigo",
+  greeting(person) {
+    let greeting = `Hello ${person.name}, my name is ${this.firstName}. `;
+    console.log(greeting + this.getCatchPhrase(person));
+  },
+  getCatchPhrase(person) {
+    return "Nice to meet you.";
+  },
+};
+
+inigo.greeting(westley);
+inigo.greeting(rugen);
