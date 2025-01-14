33e7b0cdfb1c76c615591e47a604d3ad02e2f338
ainacb <ainacb.93@gmail.com>
2025-01-14 19:46:50 +1100

    error handling added

--- module5/myapp/controllers/calculatorControllers.js
+++ module5/myapp/controllers/calculatorControllers.js
@@ -3,6 +3,13 @@
 const addNumbers = (req, res) => {
   let number1 = parseInt(req.query.num1);
   let number2 = parseInt(req.query.num2);
+
+  if (isNaN(number1) || isNaN(number2)) {
+    return res
+      .status(400)
+      .json({ error: "Invalid input:m please provide a valid number!" });
+  }
+
   let sum = number1 + number2;
   console.log(sum);
   res.status(200);
@@ -12,6 +19,13 @@ const addNumbers = (req, res) => {
 const subtractNumbers = (req, res) => {
   let number1 = parseInt(req.query.num1);
   let number2 = parseInt(req.query.num2);
+
+  if (isNaN(number1) || isNaN(number2)) {
+    return res
+      .status(400)
+      .json({ error: "Invalid input:m please provide a valid number!" });
+  }
+
   let difference = number1 - number2;
   console.log(difference);
   res.status(200);
@@ -21,6 +35,13 @@ const subtractNumbers = (req, res) => {
 const multiplyNumbers = (req, res) => {
   let number1 = parseInt(req.query.num1);
   let number2 = parseInt(req.query.num2);
+
+  if (isNaN(number1) || isNaN(number2)) {
+    return res
+      .status(400)
+      .json({ error: "Invalid input:m please provide a valid number!" });
+  }
+
   let product = number1 * number2;
   console.log(product);
   res.status(200);
@@ -30,6 +51,13 @@ const multiplyNumbers = (req, res) => {
 const divideNumbers = (req, res) => {
   let number1 = parseInt(req.query.num1);
   let number2 = parseInt(req.query.num2);
+
+  if (isNaN(number1) || isNaN(number2)) {
+    return res
+      .status(400)
+      .json({ error: "Invalid input:m please provide a valid number!" });
+  }
+
   let quotient = number1 / number2;
   console.log(quotient);
   res.status(200);
