1d2c5993d13656d5f1472dd0296434e4c5da451a
ainacb <ainacb.93@gmail.com>
2025-01-16 21:12:44 +1100

    module 5 testing

--- module5/testing/index.js
+++ module5/testing/index.js
@@ -2,6 +2,9 @@
 // import the app
 const app = require("./app");
 const port = 3000;
+const swaggerUi = require("swagger-ui-express");
+swaggerDocument = require("./swagger.json");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // start the app to listen on the right port
 app.listen(port, () => {
