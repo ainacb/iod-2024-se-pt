e55f9a030919fa6c44026fb7bbaa06538f436882
ainacb <ainacb.93@gmail.com>
2025-01-31 23:49:53 +1100

    Update BigCats.jsx

--- module6/vite-project/src/components/BigCats.jsx
+++ module6/vite-project/src/components/BigCats.jsx
@@ -1,26 +1,30 @@
 const cats = [
-    { name: 'Cheetah', latinName: 'Acinonyx jubatus' },
-    { name: 'Cougar', latinName: 'Puma concolor' },
-    { name: 'Jaguar', latinName: 'Panthera onca' },
-    { name: 'Leopard', latinName: 'Panthera pardus' },
-    { name: 'Lion', latinName: 'Panthera leo' },
-    { name: 'Snow leopard', latinName: 'Panthera uncia' },
-    { name: 'Tiger', latinName: 'Panthera tigris' },
-    ];
+  { id: 1, name: "Cheetah", latinName: "Acinonyx jubatus" },
+  { id: 2, name: "Cougar", latinName: "Puma concolor" },
+  { id: 3, name: "Jaguar", latinName: "Panthera onca" },
+  { id: 4, name: "Leopard", latinName: "Panthera pardus" },
+  { id: 5, name: "Lion", latinName: "Panthera leo" },
+  { id: 6, name: "Snow leopard", latinName: "Panthera uncia" },
+  { id: 7, name: "Tiger", latinName: "Panthera tigris" },
+];
 
 function SingleCat() {
-    return (
-        <>
-            <h1>Info about individual cat that will be mapped over</h1>
-        </>
-    );
+  return (
+    <>
+      <h1>Info about individual cat that will be mapped over</h1>
+    </>
+  );
 }
 
-
 export function BigCats() {
-    return (
-        <>
-            <h1>Cat List</h1>
-        </>
-    );
+  return (
+    <div className="BigCats componentBox">
+      <h1>Cat List</h1>
+      <ul>
+        {cats.map((cat) => (
+          <li>{cat.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
