133af3972348c476effb63b75df2945091e694b6
ainacb <ainacb.93@gmail.com>
2025-02-01 12:15:17 +1100

    module 6 continuation - movielist updated

--- module6/vite-project/src/components/MovieList.jsx
+++ module6/vite-project/src/components/MovieList.jsx
@@ -1,56 +1,75 @@
-// separate component for displaying each movie
-function Movie({title, year, synopsis}) {
-    return (
+import { useState } from "react";
+
+function Movie({ title, year, synopsis }) {
+  return (
     <li>
-        <h3>{title}</h3> <span>({year})</span>
-            <div>{synopsis}</div>
+      <h3>{title}</h3> <span>({year})</span>
+      <div>{synopsis}</div>
     </li>
-    );
-    }
-
-function MoviesList() {
-    // collection of objects representing movies
-
-    const movies = [
-    {
-    id: 1,
-    title: "The Shawshank Redemption",
-    year: 1994,
-    synopsis: "Two imprisoned men find redemption.",
-    },
-    {
-    id: 2,
-    title: "The Dark Knight",
-    year: 2008,
-    synopsis: "Batman fights the menace known as the Joker.",
-    },
-    {
-    id: 3,
-    title: "Interstellar",
-    year: 2014,
-    synopsis: "Explorers travel through a wormhole in space.",
-    },
-    ];
-
-    const movieItems = movies.map((movie, index) => (
-        <Movie 
-        key={movie.id} // key prop is required for lists
-        title={movie.title}
-        year={movie.year}
-        synopsis={movie.synopsis}
-        />
-    ));
-    
-
-    return (
-    <div className="MoviesList componentBox">
-        <ul> 
-        {" "}
-        {/* iterate over each movie, print the title in a list */}
-        {movieItems}
-        </ul>
-    </div>
-    );
+  );
 }
 
-    export { MoviesList };
+function MoviesList() {
+  // collection of objects representing movies
+
+  const movies = [
+    {
+      id: 1,
+      title: "The Shawshank Redemption",
+      year: 1994,
+      synopsis: "Two imprisoned men find redemption.",
+    },
+    {
+      id: 2,
+      title: "The Dark Knight",
+      year: 2008,
+      synopsis: "Batman fights the menace known as the Joker.",
+    },
+    {
+      id: 3,
+      title: "Interstellar",
+      year: 2014,
+      synopsis: "Explorers travel through a wormhole in space.",
+    },
+  ];
+
+  const [currentMovies, setCurrentMovies] = useState(movies);
+
+  const handleReverseMovies = () => {
+    let newMovies = [...currentMovies]; //clone or swallow copy
+    newMovies.reverse(); // reversed movies with array method
+    setCurrentMovies(newMovies); // updating state with reversed movies
+  };
+
+  const movieItems = movies.map((movie, index) => (
+    <Movie
+      key={movie.id} // key prop is required for lists
+      title={movie.title}
+      year={movie.year}
+      synopsis={movie.synopsis}
+    />
+  ));
+
+  return (
+    <>
+      {JSON.stringify(currentMovies, null, 2)}
+      <div className="MoviesList componentBox">
+        <button onClick={handleReverseMovies}>Reverse Movies</button>
+        <ul>
+          {" "}
+          {/* iterate over each movie, print the title in a list */}
+          {/* {movieItems} */}
+          {currentMovies.map((movie) => (
+            <Movie
+              key={movie.id} // key prop is required for lists
+              title={movie.title}
+              year={movie.year}
+              synopsis={movie.synopsis}
+            />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+export { MoviesList };
