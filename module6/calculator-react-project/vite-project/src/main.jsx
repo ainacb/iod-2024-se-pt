ce25cae4c5e3f8e57dffc55d1f333d122fb080e9
ainacb <ainacb.93@gmail.com>
2025-02-04 20:02:57 +1100

    module 6 lab 6 cont

--- module6/calculator-react-project/vite-project/src/main.jsx
+++ module6/calculator-react-project/vite-project/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ErrorMessage from "./components/ErrorMessage.jsx";
+import { ErrorBoundary } from "react-error-boundary";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <ErrorBoundary FallbackComponent={ErrorMessage}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ErrorBoundary>
+);
