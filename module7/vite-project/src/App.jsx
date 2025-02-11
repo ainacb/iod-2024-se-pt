91fa4df53739d02704a01460973b60b06d95cccd
ainacb <ainacb.93@gmail.com>
2025-02-11 21:04:11 +1100

    module 7 updated

--- module7/vite-project/src/App.jsx
+++ module7/vite-project/src/App.jsx
@@ -3,17 +3,30 @@ import ClockDisplay from "./components/ClockDisplay";
 import { ActivityFinder } from "./components/ActivityFinder";
 import { BitcoinRates } from "./components/BitcoinRates";
 import Charts from "./components/Charts";
+import RefCounter from "./components/RefCounter";
+import VideoPlayer from "./components/VideoPlayer";
+import ReducerCounter from "./components/ReducerCounter";
+import PostListReducer from "./components/PostListReducer";
 
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <ActivityFinder />
+
+      {/* <PostListReducer /> */}
+
+      {/* <ReducerCounter /> */}
+
+      {/* <VideoPlayer /> */}
+
+      {/* <RefCounter /> */}
+
+      {/* <ActivityFinder /> */}
 
       <BitcoinRates />
       {/* <ClockDisplay /> */}
 
-      <Charts />
+      {/* <Charts /> */}
     </>
   );
 }
